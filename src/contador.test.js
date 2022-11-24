19b73a476c113f66fd2832cede4c335a172a89dd
Alfred <alfred.garcia@ucb.edu.bo>
2022-11-24 10:25:38 -0400

    2ª TEST

--- src/contador.test.js
+++ src/contador.test.js
@@ -5,13 +5,13 @@ describe("CONTADOR VACIO", () => {
     expect(contar(" ")).toEqual(0);
   });
 });
-/*
+
 describe("CONTADOR DE 1 LETRA", () => {
   it("deberia retornar 1 de dicha letra", () => {
     expect(contar("a")).toEqual(1);
   });
 });
-
+/*
 describe("CONTADOR DE CADENA", () => {
   it("deberia retornar 3 de dicha cadena de letra a", () => {
     expect(contar("palabra")).toEqual(3);
