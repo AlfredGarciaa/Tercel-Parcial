0743cf7c1a8d003d3185c53d6fac9721251de29b
Alfred <alfred.garcia@ucb.edu.bo>
2022-11-24 09:22:52 -0400

    2ª criterio de confirmacion cumplido

--- cypress/e2e/twitter.cy.js
+++ cypress/e2e/twitter.cy.js
@@ -5,4 +5,11 @@ describe("SITIO WEB DE TWITTER", () => {
     cy.get("#button").click();
     cy.get("#publicacion-div").should("contain", "'Hola mundo'");
   });
+
+  it("Deberia mandar un mensaje de texto si esta vacio y visualizarlo", () => {
+    cy.visit("/");
+    cy.get("#texto").type(" ");
+    cy.get("#button").click();
+    cy.get("#publicacion-div").should("contain", "'No se puede ingresar un post sin texto'");
+  });
 });
--- src/presenter.js
+++ src/presenter.js
@@ -2,10 +2,18 @@ const texto = document.querySelector("#texto");
 const post = document.querySelector("#post-form");
 const publicacion = document.querySelector("#publicacion-div");
 
+const vacio = " ";
+const mensaje = "'No se puede ingresar un post sin texto'";
+
 post.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const cadena = texto.value;
 
-  publicacion.innerHTML = "<p>" + cadena + "</p>";
+  if(cadena == vacio){
+    publicacion.innerHTML = "<p>" + mensaje + "</p>";
+  }
+  else {
+    publicacion.innerHTML = "<p>" + cadena + "</p>";
+  }
 });
