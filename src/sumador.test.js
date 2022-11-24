1d6d05cc5bdba5b9520a2728a689630dd2127964
Alfred <alfred.garcia@ucb.edu.bo>
2022-11-24 09:27:24 -0400

    Prueba vacio pasando

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -2,6 +2,6 @@ import sumar from "./sumador.js";
 
 describe("CONTADOR VACIO", () => {
   it("deberia retornar 0 si no hay palabra", () => {
-    expect(sumar("")).toEqual(0);
+    expect(sumar(" ")).toEqual(0);
   });
 });
